4abd6fc74e28bb99afe62d1d513dcaff7aefdfb0
0x142857 <0x142857@gmail.com>
2015-02-17 08:41:51 +0800

    remove the GitHunt part, feel it too buggy :disappointed:

--- chrome/js/background.js
+++
@@ -1,8 +0,0 @@
-// action when install
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        window.open('https://githunt.io/user/signin/github?from=extension');
-    }else if(details.reason == "update"){
-        // redirect somewhere when update
-    }
-});
